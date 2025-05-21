ccf1a5b59e31b23bee52951cc668460fd53e7209
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-05-21 19:55:07 +0300

    Create exercise1.js

---
+++ exercise1.js
@@ -0,0 +1,11 @@
+// Exercise 1: Conditional Logic Practice
+
+const number = 5; // You can change this value
+
+if (number > 0) {
+  console.log(`${number} is positive`);
+} else if (number < 0) {
+  console.log(`${number} is negative`);
+} else {
+  console.log(`${number} is zero`);
+}
