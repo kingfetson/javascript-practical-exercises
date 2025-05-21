fa106e8a77ede4bd2d7b7e89020d640de6bff568
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-05-21 19:58:38 +0300

    Create exercise3.js
    Function to Add Two Numbers

---
+++ exercise3.js
@@ -0,0 +1,8 @@
+// Exercise 3: Basic Function Creation
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+const result = sum(7, 3); // You can change the numbers
+console.log(`The sum is: ${result}`);
